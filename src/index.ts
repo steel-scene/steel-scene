7c79f84b85adbdacf2ea001710468dbb5f20a80e
Christopher Wallis <supercryptonite@gmail.com>
2017-02-21 22:24:30 -0500

    added type exports

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,12 @@ export const {
 export const importJSON = bu.importJSON as { (layers: IDictionary<ILayer>, reset: boolean): BlueUnicorn };
 export const use = bu.use as { (a: IAnimationEngine): BlueUnicorn };
 
+/**
+ * Export all types to "types"
+ */
+import * as types from './types';
+export { types };
+
 // auto-wire up document on DOMContentLoaded
 if (window && window.document) {
   document.addEventListener('DOMContentLoaded', () => {
